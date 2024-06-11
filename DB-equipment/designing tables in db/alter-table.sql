3c0f2a95c9fadf6989747b2113a4ed75c1923b28
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2024-06-11 14:37:48 +0200

    added new script about changing existed tables

--- DB-equipment/designing tables in db/alter-table.sql
+++ DB-equipment/designing tables in db/alter-table.sql
@@ -1,7 +1,23 @@
 ----------------------------------
---changes in vendor table
+--changes in existed tables
 ----------------------------------
 ALTER TABLE vendor
 ADD is_favourite BOOLEAN DEFAULT false;
 
 SELECT * FROM vendor
+
+ALTER TABLE product
+ADD price NUMERIC(10,2) NULL,
+ADD quantity SMALLINT DEFAULT 1;
+
+SELECT * FROM product
+
+
+ALTER TABLE card RENAME TO varranty;
+ALTER TABLE varranty RENAME card_id TO varranty_id;
+
+ALTER TABLE varranty ALTER COLUMN scope SET DATA TYPE VARCHAR(20);
+
+SELECT * FROM varranty;
+
+ALTER TABLE varranty DROP COLUMN scope;
