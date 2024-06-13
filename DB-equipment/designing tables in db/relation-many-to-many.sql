535e809fb3da8e76b8773b728567d21f3adc7eb2
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2024-06-13 11:15:58 +0200

    code regarding testing relationship of the table to itself

--- DB-equipment/designing tables in db/relation-many-to-many.sql
+++ DB-equipment/designing tables in db/relation-many-to-many.sql
@@ -29,3 +29,29 @@ FROM owner o
 JOIN owner_product op ON o.owner_id = op.owner_id
 JOIN product p ON p.product_id = op.product_id
 
+---
+--relation table to itself 
+--
+
+CREATE TABLE task
+(
+	task_id INT PRIMARY KEY,
+	name TEXT NOT NULL,
+	parent_task_id INT REFERENCES task
+)
+
+INSERT INTO task
+VALUES 
+(1, 'Finish course', NULL),
+(2, 'Finish course 1', 1),
+(3, 'Finish course 2', 1),
+(4, 'Finish course 1', 2),
+(5, 'Finish course 2', 2),
+(6, 'Finish course 11', 3),
+(7, 'Finish course 12', 3)
+
+SELECT * FROM task
+SELECT * FROM task WHERE parent_task_id=1;
+SELECT * FROM task WHERE parent_task_id=2;
+SELECT * FROM task WHERE parent_task_id=3;
+SELECT * FROM task WHERE parent_task_id IS NULL;
